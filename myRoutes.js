3ea7ad1f7497e80e82a47e0d79ccd319309b70ef
Des Whelan <wheland6@tcd.ie>
2019-03-24 13:12:13 +1300

    use sendFile in server.js

--- myRoutes.js
+++ myRoutes.js
@@ -1,8 +1,12 @@
 module.exports = {
-    myIndex: myIndex
+    myIndex: myIndex,
+    cavan: cavan
 }
 
 function myIndex (req, res) {
     res.send('<p>Hello Simey it\'s me again Simey. I\'m your worst nightmare</p>')
 }
 
+function cavan (req, res) {
+    res.sendFile(__dirname+'/simey.html')
+}
--- myServer.js
+++ myServer.js
@@ -4,12 +4,9 @@ const myRoutes = require('./myRoutes')
 const port = 3000
 const server = express()
 
-server.get('/', myRoutes.myIndex)
+server.get('/', myRoutes.myIndex);
 
-// server.get('/', (req, res) => {
-//   res.send('<h1>Hello Simey</h1>')
-// })
-
+server.get('/cavan', myRoutes.cavan);
 
 server.listen(port, () => {
   console.log('The server is listening on port', port)
